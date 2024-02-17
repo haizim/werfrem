ebb7941af70aa57eb7c7bf9743c9484594d6d3f4
wahid <wahihasyim@gmail.com>
2024-02-17 15:55:56 +0700

    penambahan directive switch, for dan while

--- app/Core/View.php
+++ app/Core/View.php
@@ -13,11 +13,26 @@ class View
 
         $_combined = preg_replace('/{{\s*(.+?)\s*}}/', '<?php echo $1 ?>', $_combined);
 
+        // if
         $_combined = preg_replace('/@if\(\s*(.+?)\s*\):/', '<?php if ($1) { ?>', $_combined);
+        $_combined = preg_replace('/@elseif\(\s*(.+?)\s*\):/', '<?php } elseif ($1) { ?>', $_combined);
         $_combined = str_replace('@endif', '<?php } ?>', $_combined);
+        // switch
+        $_combined = preg_replace('/@switch\(\s*(.+?)\s*\):/', '<?php switch ($1) { ?>', $_combined);
+        $_combined = preg_replace('/@case\(\s*(.+?)\s*\):/', '<?php case "$1" : ?>', $_combined);
+        $_combined = str_replace('@default', '<?php default : ?>', $_combined);
+        $_combined = str_replace('@break', '<?php break; ?>', $_combined);
+        $_combined = str_replace('@endswitch', '<?php } ?>', $_combined);
         
+        // for
+        $_combined = preg_replace('/@for\(\s*(.+?)\s*\):/', '<?php for ($1) { ?>', $_combined);
+        $_combined = str_replace('@endfor', '<?php } ?>', $_combined);
+        // foreach
         $_combined = preg_replace('/@foreach\(\s*(.+?)\s*\):/', '<?php foreach ($1) { ?>', $_combined);
         $_combined = str_replace('@endforeach', '<?php } ?>', $_combined);
+        // while
+        $_combined = preg_replace('/@while\(\s*(.+?)\s*\):/', '<?php while ($1) { ?>', $_combined);
+        $_combined = str_replace('@endwhile', '<?php } ?>', $_combined);
 
         unset($content);
         unset($template);
